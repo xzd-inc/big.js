8f1fc292b282d85efa9acc1610176645dd54f02b
xuzhendong <wb-xzd797743@alibaba-inc.com>
2022-06-17 11:37:44 +0800

    feat: 初始化

---
+++ big.ts
@@ -0,0 +1,19 @@
+class Big {
+  /* 基础配置 */
+  static DP = 20
+  static RM = 1
+  static MAX_DP = 1E6
+  static MAX_POWER = 1E6
+  static NE = -7
+  static PE = 21
+  static STRICT = false
+
+  /* 报错信息 */
+  static NAME = '[big.js] '
+  static INVALID = Big.NAME + 'Invalid '
+  static INVALID_DP = Big.INVALID + 'decimal places'
+  static INVALID_RM = Big.INVALID + 'rounding mode'
+  static DIV_BY_ZERO = Big.NAME + 'Division by zero'
+
+  
+}
